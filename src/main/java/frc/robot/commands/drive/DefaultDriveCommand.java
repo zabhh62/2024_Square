acf644e24ea24286463c899f9485f0192c485951
Richard McMullin <richard.e.mcmullin@gmail.com>
2023-11-11 08:08:47 -0500

    Initial code

---
+++ src/main/java/frc/robot/commands/drive/DefaultDriveCommand.java
@@ -0,0 +1,91 @@
+package frc.robot.commands.drive;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import frc.robot.Constants.DriveConstants.DriveMode;
+import frc.robot.commands.LoggingCommandBase;
+import frc.robot.operator.GameController;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class DefaultDriveCommand extends LoggingCommandBase {
+
+    private final DriveSubsystem             driveSubsystem;
+    private final XboxController             driverController;
+    private final SendableChooser<DriveMode> driveModeChooser;
+
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param driveSubsystem The subsystem used by this command.
+     */
+    public DefaultDriveCommand(GameController driverController, SendableChooser<DriveMode> driveModeChooser,
+        DriveSubsystem driveSubsystem) {
+
+        this.driverController = driverController;
+        this.driveModeChooser = driveModeChooser;
+        this.driveSubsystem   = driveSubsystem;
+
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(driveSubsystem);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        logCommandStart();
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+
+        DriveMode driveMode = driveModeChooser.getSelected();
+
+        boolean   boost     = driverController.getRightBumper();
+
+        switch (driveMode) {
+
+        case DUAL_STICK_ARCADE:
+            setMotorSpeedsArcade(driverController.getLeftY(), driverController.getRightX(), boost);
+            break;
+
+        case SINGLE_STICK_ARCADE:
+            setMotorSpeedsArcade(driverController.getLeftY(), driverController.getLeftX(), boost);
+            break;
+
+        case TANK:
+        default:
+
+            if (boost) {
+                driveSubsystem.setMotorSpeeds(driverController.getLeftY(), driverController.getRightY());
+            }
+            else {
+                // If not in boost mode, then divide the motors speeds in half
+                driveSubsystem.setMotorSpeeds(driverController.getLeftY() / 2.0, driverController.getRightY() / 2.0);
+            }
+            break;
+        }
+
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        // The default drive command never ends, but can be interrupted by other commands.
+        return false;
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        logCommandEnd(interrupted);
+    }
+
+    private void setMotorSpeedsArcade(double speed, double turn, boolean boost) {
+
+        // FIXME: what should we put here?
+        driveSubsystem.setMotorSpeeds(0, 0);
+    }
+
+
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,128 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
+
+public class DriveSubsystem extends SubsystemBase {
+
+    // The motors on the left side of the drive.
+    private final TalonSRX     leftPrimaryMotor         = new TalonSRX(DriveConstants.LEFT_MOTOR_PORT);
+    private final TalonSRX     leftFollowerMotor        = new TalonSRX(DriveConstants.LEFT_MOTOR_PORT + 1);
+
+    // The motors on the right side of the drive.
+    private final TalonSRX     rightPrimaryMotor        = new TalonSRX(DriveConstants.RIGHT_MOTOR_PORT);
+    private final TalonSRX     rightFollowerMotor       = new TalonSRX(DriveConstants.RIGHT_MOTOR_PORT + 1);
+
+    private final DigitalInput targetSensor             = new DigitalInput(0);
+
+    // Conversion from volts to distance in cm
+    // Volts distance
+    // 0.12 30.5 cm
+    // 2.245 609.6 cm
+    private final AnalogInput  ultrasonicDistanceSensor = new AnalogInput(0);
+
+    private final double       ULTRASONIC_M             = (609.6 - 30.5) / (2.245 - .12);
+    private final double       ULTRASONIC_B             = 609.6 - ULTRASONIC_M * 2.245;
+
+
+    private double             leftSpeed                = 0;
+    private double             rightSpeed               = 0;
+
+    private AHRS               navXGyro                 = new AHRS() {
+                                                            // Override the "Value" in the gyro
+                                                            // sendable to use the angle instead of
+                                                            // the yaw.
+                                                            // Using the angle makes the gyro appear
+                                                            // in the correct position accounting
+                                                            // for the
+                                                            // offset. The yaw is the raw sensor
+                                                            // value which appears incorrectly on
+                                                            // the dashboard.
+                                                            @Override
+                                                            public void initSendable(SendableBuilder builder) {
+                                                                builder.setSmartDashboardType("Gyro");
+                                                                builder.addDoubleProperty("Value", this::getAngle, null);
+                                                            }
+                                                        };
+
+    /** Creates a new DriveSubsystem. */
+    public DriveSubsystem() {
+
+        // We need to invert one side of the drivetrain so that positive voltages
+        // result in both sides moving forward. Depending on how your robot's
+        // gearbox is constructed, you might have to invert the left side instead.
+        leftPrimaryMotor.setInverted(DriveConstants.LEFT_MOTOR_REVERSED);
+        leftFollowerMotor.setInverted(DriveConstants.LEFT_MOTOR_REVERSED);
+
+        leftPrimaryMotor.setNeutralMode(NeutralMode.Brake);
+        leftFollowerMotor.setNeutralMode(NeutralMode.Brake);
+
+        leftFollowerMotor.follow(leftPrimaryMotor);
+
+
+        rightPrimaryMotor.setInverted(DriveConstants.RIGHT_MOTOR_REVERSED);
+        rightFollowerMotor.setInverted(DriveConstants.RIGHT_MOTOR_REVERSED);
+
+        rightPrimaryMotor.setNeutralMode(NeutralMode.Brake);
+        rightFollowerMotor.setNeutralMode(NeutralMode.Brake);
+
+        rightFollowerMotor.follow(rightPrimaryMotor);
+    }
+
+    public double getUltrasonicDistanceCm() {
+
+        double ultrasonicVoltage = ultrasonicDistanceSensor.getVoltage();
+
+        double distanceCm        = ULTRASONIC_M * ultrasonicVoltage + ULTRASONIC_B;
+
+        return Math.round(distanceCm);
+    }
+
+    /**
+     * Set the left and right speed of the primary and follower motors
+     *
+     * @param leftSpeed
+     * @param rightSpeed
+     */
+    public void setMotorSpeeds(double leftSpeed, double rightSpeed) {
+
+        this.leftSpeed  = leftSpeed;
+        this.rightSpeed = rightSpeed;
+
+        leftPrimaryMotor.set(ControlMode.PercentOutput, leftSpeed);
+        rightPrimaryMotor.set(ControlMode.PercentOutput, rightSpeed);
+
+        // NOTE: The follower motors are set to follow the primary
+        // motors
+    }
+
+    /** Safely stop the subsystem from moving */
+    public void stop() {
+        setMotorSpeeds(0, 0);
+    }
+
+    public boolean isTargetDetected() {
+        return !targetSensor.get();
+    }
+
+    @Override
+    public void periodic() {
+
+        SmartDashboard.putNumber("Right Motor", rightSpeed);
+        SmartDashboard.putNumber("Left  Motor", leftSpeed);
+
+        SmartDashboard.putNumber("Ultrasonic Voltage", ultrasonicDistanceSensor.getVoltage());
+        SmartDashboard.putNumber("Ultrasonic Distance (cm)", getUltrasonicDistanceCm());
+
+        SmartDashboard.putData("Gyro", navXGyro);
+    }
+}
